fa0486a432dbb649ef5b2bf78425d948ea02e692
boraarat <boraarat@gmail.com>
2019-06-14 15:20:43 +0300

    Office 365 Mail Expand

---
+++ How to Auto-Expand Exchange Online Archives.ps1
@@ -0,0 +1,17 @@
+﻿
+Set-ExecutionPolicy RemoteSigned
+
+$UserCredential = Get-Credential
+
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+
+Import-PSSession $Session -DisableNameChecking
+
+Set-OrganizationConfig -AutoExpandingArchive
+
+Get-OrganizationConfig | FL AutoExpandingArchiveEnabled
+
+Enable-Mailbox ahmet.d@linkshipping.com -AutoExpandingArchive
+
+Get-Mailbox ahmet.d@linkshipping.com | FL AutoExpandingArchiveEnabled 
+
